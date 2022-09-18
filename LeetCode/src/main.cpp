adfb141d6d6c87577fd8f6fad940a3320f516822
Robotislove <17320492563@163.com>
2022-09-18 17:39:40 +0800

    first init

---
+++ LeetCode/src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: Robotislove 17320492563@163.com
+ * @Date: 2022-09-18 10:58:33
+ * @LastEditors: Robotislove 17320492563@163.com
+ * @LastEditTime: 2022-09-18 17:26:53
+ * @FilePath: \code\TemplateRepoCxx\src\a.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include<iostream>
+#include<string>
+using namespace std;
+/*
+string::npos表示没找到
+  */
+int main(){
+    
+}
